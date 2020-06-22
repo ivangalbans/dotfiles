40a1909b1b2d816f7bb9da0e658d9fe3a85929c0
Ivan Galban <ivan.galban.smith@gmail.com>
2020-06-22 15:58:11 -0400

    Add emacs config

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default"   . ((user-emacs-directory . "~/.doom.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/.spacemacs.emacs.d")))
+ ("doom"      . ((user-emacs-directory . "~/.doom.emacs.d"))))
