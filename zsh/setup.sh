8988bb8b1ab18cbaf9d032748a3beb27b9289965
Ivan Galban <ivan.galban.smith@gmail.com>
2020-06-25 22:54:44 -0400

    Add zsh configs

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -12,11 +12,11 @@ info "Configurating zsh..."
 
 # info "Pulling custom theme and plugins"
 git clone https://github.com/romkatv/powerlevel10k.git             "$SOURCE/ohmyzsh/custom/themes/powerlevel10k"
-# git clone https://github.com/inishchith/autoenv.git                "$SOURCE/ohmyzsh/custom/plugins/autoenv"
-# git clone https://github.com/zsh-users/zsh-apple-touchbar.git      "$SOURCE/ohmyzsh/custom/plugins/zsh-apple-touchbar"
-# git clone https://github.com/zsh-users/zsh-autosuggestions.git     "$SOURCE/ohmyzsh/custom/plugins/zsh-autosuggestions"
-# git clone https://github.com/zsh-users/zsh-completions.git         "$SOURCE/ohmyzsh/custom/plugins/zsh-completions"
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$SOURCE/ohmyzsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/inishchith/autoenv.git                "$SOURCE/ohmyzsh/custom/plugins/autoenv"
+git clone https://github.com/zsh-users/zsh-apple-touchbar.git      "$SOURCE/ohmyzsh/custom/plugins/zsh-apple-touchbar"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git     "$SOURCE/ohmyzsh/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-completions.git         "$SOURCE/ohmyzsh/custom/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$SOURCE/ohmyzsh/custom/plugins/zsh-syntax-highlighting"
 #
 
 find . -d 1 -name "*.zsh*" | while read fn; do
