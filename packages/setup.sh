3c68d54f29339967bd5257e2a4d602c5df9d69b0
Ivan Galban <ivan.galban.smith@gmail.com>
2020-07-04 17:12:13 -0400

    Add packages for npm and pip

--- packages/setup.sh
+++ packages/setup.sh
@@ -8,10 +8,22 @@ cd "$DIR"
 
 info "Installing Brewfile packages..."
 brew bundle
+success "Finished installing Brewfile packages."
 
-info "Installing nvm, node, npm"
+info "Installing nvm, node"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node
-npm install npm@latest -g
+
+find * -name "*.list" | while read fn; do
+    cmd="${fn%.*}"
+    set -- $cmd
+    info "Installing $1 packages..."
+    while read package; do
+        if [[ $package == $COMMENT ]]; then continue; fi
+        substep_info "Installing $package..."
+        $cmd $package
+    done < "$fn"
+    success "Finished installing $1 packages."
+done
 
 success "All packages were installed"
