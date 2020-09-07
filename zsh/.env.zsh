5e3ccbf485017f06e425abd747b3c1b96e84256e
Ivan Galban <ivan.galban.smith@gmail.com>
2020-09-07 14:05:31 -0400

    Add omnisharp to path and update doom

--- zsh/.env.zsh
+++ zsh/.env.zsh
@@ -22,5 +22,6 @@ export PATH="$PATH:/Users/ivan/.dotnet/tools"
 export PATH="$PATH:/Users/ivan/Library/Python/3.7/bin"
 export PATH="$PATH:$HOME/.doom.emacs.d/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/usr/local/bin/omnisharp"
 
 GITSTATUS_LOG_LEVEL=DEBUG
