91f4776f7e7f1d76c04e9d21c114f2d6f48d41e9
Ivan Galban <ivan.galban.smith@gmail.com>
2020-09-14 11:28:52 -0400

    Add alias for music

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
 alias ls="colorls"
+alias ml="music launch"
+alias mp="music play"
+alias ms="music pause"
+alias mm="music mute"
+alias mu="music unmute"
+alias mn="music next"
