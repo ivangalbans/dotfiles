7001a922ad99e187796bfa9b97be420b2ef68eeb
Ivan Galban <ivan.galban.smith@gmail.com>
2021-05-21 14:24:11 -0500

    Add alias for nu refresh

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -8,5 +8,22 @@ alias mm="music mute"
 alias mu="music unmute"
 alias mn="music next"
 alias rm="safe-rm"
-alias nu-refresh="nu aws credentials refresh --okta"
-alias klj="clj -M:klipse"
+alias klj='ff(){ clj "$@" -M:klipse;  unset -f ff; }; ff'
+
+nu-refresh() {
+    nu update
+    echo 0 | nu aws credentials refresh
+    nu certs setup --env staging
+    nu certs setup --env prod
+    nu auth get-refresh-token --env prod
+    nu auth get-refresh-token --env staging
+    nu auth get-access-token --env prod
+    nu auth get-access-token --env staging
+    
+    nu-mx certs setup --env staging
+    nu-mx certs setup --env prod
+    nu-mx auth get-refresh-token --env prod
+    nu-mx auth get-refresh-token --env staging
+    nu-mx auth get-access-token --env prod
+    nu-mx auth get-access-token --env staging
+}
