53e28e0232488ebdacfc025fed3138ff28cef401
Ivan Galban <ivan.galban.smith@gmail.com>
2022-07-02 16:10:30 -0500

    Update doom emacs repo

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -17,11 +17,6 @@ info "Installing Emacs"
 brew tap railwaycat/emacsmacport
 brew install --cask emacs-mac
 
-# from https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#with-homebrew
-# brew tap railwaycat/emacsmacport
-# brew install emacs-mac --with-modules
-# ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
-
 info "Linking emacs profiles..."
 
 find . -name "*.d" | while read fn; do
