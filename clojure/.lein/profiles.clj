0020b56b1e6903baab4946dd2c431740184f131b
Ivan Galban <ivan.galban.smith@gmail.com>
2022-07-02 15:58:56 -0500

    Update clojure-lsp, clj-kondo and lein profiles

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -4,19 +4,18 @@
                         ["clojars"  {:url "https://clojars.org/repo/"}]
                         ["nu-maven" {:url "s3p://nu-maven/releases/" :region "sa-east-1"}]]
   :plugin-repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
-  :plugins             [[s3-wagon-private "1.3.3"]
-                        [com.jakemccrary/lein-test-refresh "0.24.1"]
-                        [lein-ancient "0.6.15"]]}
+  :jvm-opts            ["-Djava.locale.providers='COMPAT,JRE,CLDR'"]
+  :plugins             [[s3-wagon-private "1.3.5"]
+                        [com.jakemccrary/lein-test-refresh "0.25.0"]
+                        [lein-ancient "0.7.1-SNAPSHOT"]]}
 
- :test {:dependencies [[cljdev "0.9.0"]]
+ :test {:dependencies [[cljdev "0.10.2"]]
         :injections   [(require 'nu)]}
 
  :repl {;; If you need to have `cider-nrepl` and `refactor-nrepl` in your REPL
         ;; session (i.e.: vim-users), uncomment the following lines
-        :plugins [[cider/cider-nrepl "0.26.0"]
-                  [refactor-nrepl    "3.0.0-pr301"]]
+        ;; :plugins [[cider/cider-nrepl "0.25.3"]
+        ;;           [refactor-nrepl "2.5.0-SNAPSHOT"]]
         :repl-options {:timeout 300000}
-        :dependencies [[cljdev "0.9.0"]
-                       [com.cognitect/rebl            "0.9.242"]]
-        :injections   [(require 'nu)
-                       (require 'cognitect.rebl)]}}
+        :dependencies [[cljdev "0.10.2"]]
+        :injections   [(require 'nu)]}}
